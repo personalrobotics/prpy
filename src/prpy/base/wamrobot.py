27c2887bedfceff06f1c2a700190d4ea55140541
Jennifer King <jeking@cs.cmu.edu>
2013-09-17 22:00:15 +0000

    Adding missing import of numpy

--- src/prpy/base/wamrobot.py
+++ src/prpy/base/wamrobot.py
@@ -28,7 +28,7 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
-import openravepy
+import openravepy, numpy
 from .. import util
 from robot import Robot
 import manipulation2.trajectory, prpy.rave
