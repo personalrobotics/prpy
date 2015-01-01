9909ca28be4d0aebccc6b36e966ec680a6e46a78
Michael Koval <mkoval@cs.cmu.edu>
2015-01-01 03:50:40 -0500

    Added a few more SnapPlanner tests.

--- tests/test_SnapPlanner.py
+++ tests/test_SnapPlanner.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python
-import numpy
+import numpy, numpy.testing
 import openravepy
 import unittest
 from prpy.planning.base import PlanningError
 from prpy.planning.snap import SnapPlanner
 
 class SnapPlannerTest(unittest.TestCase):
+    # This configuration is in collision with the environment, but is not in
+    # self-collision.
     config_env_collision = numpy.array([
         3.63026273e-01,  -1.54688036e+00,  -1.30000000e+00,
         2.34703418e+00,   3.28152338e-01,  -1.10662864e+00,
        -2.07807269e-01
     ])
+
+    # This configuration is in self-collision, but is not in collision with the
+    # environment.
     config_self_collision = numpy.array([
         2.60643264e+00,   1.97222205e+00,  -8.24298541e-16,
         2.79154009e+00,   1.30899694e+00,  -4.71027738e-16,
         0.00000000e+00
     ])
+
+    # These waypoints have the following relationship:
+    # - all three configurations are collision-free
+    # - (config_segment1, config_segment2) in collision with the environment
+    # - (config_segment2, config_segment3) is collision free.
+    # - both segments are self-collision free
     config_segment1 = numpy.array([
         1.23760308e-01,   6.05769772e-01,  -5.00000000e-02,
         1.33403907e+00,   4.47724617e-01,  -3.14811779e-01,
@@ -26,7 +37,12 @@ class SnapPlannerTest(unittest.TestCase):
         1.33403907e+00,   4.47724617e-01,  -3.14811779e-01,
        -1.90265540e+00
     ])
-    
+    config_segment3 = numpy.array([
+        1.50376031e+00,   6.05769772e-01,  -5.00000000e-02,
+        1.33403907e+00,   4.47724617e-01,  -3.14811779e-01,
+       -1.90265540e+00
+    ])
+
     def setUp(self):
         self.env = openravepy.Environment()
         self.env.Load('data/wamtest2.env.xml')
@@ -49,7 +65,45 @@ class SnapPlannerTest(unittest.TestCase):
             # radius. This will be used to test endpoint conditions.
             dof_resolutions = self.robot.GetActiveDOFResolutions()
             self.config_epsilon = numpy.zeros(len(dof_resolutions))
-            self.config_epsilon[0] += 2 * dof_resolutions[0]
+            self.config_epsilon[0] += 0.5 * dof_resolutions[0]
+
+    def test_PlanToConfiguration_LineIsValid_ReturnsTrajectory(self):
+        # Setup and sanity checks.
+        self.robot.SetActiveDOFValues(self.config_segment3)
+        self.assertFalse(self.env.CheckCollision(self.robot))
+        self.assertFalse(self.robot.CheckSelfCollision())
+
+        self.robot.SetActiveDOFValues(self.config_segment2)
+        self.assertFalse(self.env.CheckCollision(self.robot))
+        self.assertFalse(self.robot.CheckSelfCollision())
+
+        # Test.
+        traj = self.planner.PlanToConfiguration(self.robot, self.config_segment3)
+
+        # Assert.
+        self.assertEqual(traj.GetNumWaypoints(), 2)
+        self.assertEqual(traj.GetConfigurationSpecification(),
+                         self.robot.GetActiveConfigurationSpecification())
+        numpy.testing.assert_allclose(traj.GetWaypoint(0), self.config_segment2)
+        numpy.testing.assert_allclose(traj.GetWaypoint(1), self.config_segment3)
+
+    def test_PlanToConfiguration_WithinResolution_ReturnsTrajectory(self):
+        goal_config = self.config_segment1 + self.config_epsilon
+
+        # Setup and sanity checks.
+        self.robot.SetActiveDOFValues(self.config_segment1)
+        self.assertFalse(self.env.CheckCollision(self.robot))
+        self.assertFalse(self.robot.CheckSelfCollision())
+
+        # Test.
+        traj = self.planner.PlanToConfiguration(self.robot, goal_config)
+
+        # Assert.
+        self.assertEqual(traj.GetNumWaypoints(), 2)
+        self.assertEqual(traj.GetConfigurationSpecification(),
+                         self.robot.GetActiveConfigurationSpecification())
+        numpy.testing.assert_allclose(traj.GetWaypoint(0), self.config_segment1)
+        numpy.testing.assert_allclose(traj.GetWaypoint(1), goal_config)
 
     def test_PlanToConfiguration_StartInCollision_ThrowsPlanningError(self):
         # Setup and sanity checks.
