b3bd301eff8dbb9842a66646f9357bf2660549b1
Michael Koval <mkoval@cs.cmu.edu>
2015-01-05 15:20:59 -0500

    PEP8 fixes.

--- tests/test_Planning.py
+++ tests/test_Planning.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python
-import openravepy, unittest, numpy
-from openravepy import (Environment, IkFilterOptions, IkParameterization,
-                        IkParameterizationType)
+import openravepy
+import unittest
+import numpy
+from openravepy import Environment
 from prpy.planning.base import PlanningError
 from prpy.planning.cbirrt import CBiRRTPlanner
 from prpy.planning.ompl import OMPLPlanner, OMPLSimplifier
@@ -14,6 +15,8 @@ VerifyTrajectory = openravepy.planningutils.VerifyTrajectory
 #
 # This class is the base class of all planner tests. It is responsible for
 # setting up the environment, but does directly not implement any tests.
+
+
 class BasePlannerTest(object):
     """Generic environment setup.
     """
@@ -21,20 +24,20 @@ class BasePlannerTest(object):
 
     # Feasible start/goal pair.
     config_feasible_start = numpy.array([
-         2.35061574,  0.61043555,  0.85      ,  1.80684444, -0.08639935,
+        +2.35061574,  0.61043555,  0.85000000,  1.80684444, -0.08639935,
         -0.69750474,  1.31656172
     ])
     config_feasible_goal = numpy.array([
-        -0.84085883,  1.44573701,  0.2       ,  1.72620231, -0.81124757,
+        -0.84085883,  1.44573701,  0.20000000,  1.72620231, -0.81124757,
         -1.39363597,  1.29233111
     ])
 
     # This configuration is in collision with the environment, but is not in
     # self-collision.
     config_env_collision = numpy.array([
-        3.63026273e-01,  -1.54688036e+00,  -1.30000000e+00,
-        2.34703418e+00,   3.28152338e-01,  -1.10662864e+00,
-       -2.07807269e-01
+        +3.63026273e-01,  -1.54688036e+00,  -1.30000000e+00,
+        +2.34703418e+00,   3.28152338e-01,  -1.10662864e+00,
+        -2.07807269e-01
     ])
 
     # This configuration is in self-collision, but is not in collision with the
@@ -45,26 +48,25 @@ class BasePlannerTest(object):
         0.00000000e+00
     ])
 
-    # Waypoints that can be pair-wise connected by feasible straight line paths.
+    # Waypoints that can be pair-wise connected by straight line paths.
     waypoint1 = numpy.array([
         1.12376031,  0.60576977, -0.05000000,
         1.33403907,  0.44772461, -0.31481177,
-       -1.90265540
+        1.90265540
     ])
     waypoint2 = numpy.array([
-        1.53181533,  0.80270404, -0.05      ,
+        1.53181533,  0.80270404, -0.05000000,
         1.75341989,  0.21348846, -0.91026757,
-       -1.59603932
+        1.59603932
     ])
     waypoint3 = numpy.array([
         1.50376031,  0.60576977, -0.05000000,
         1.33403907,  0.44772461, -0.31481177,
-       -1.90265540
+        1.90265540
     ])
 
-
     def setUp(self):
-        self.env = openravepy.Environment()
+        self.env = Environment()
         self.env.Load('data/wamtest2.env.xml')
         self.robot = self.env.GetRobot('BarrettWAM')
         self.manipulator = self.robot.GetManipulator('arm')
@@ -108,8 +110,8 @@ class BasePlannerTest(object):
         for iwaypoint in xrange(1, traj.GetNumWaypoints() - 1):
             curr_waypoint = traj.GetWaypoint(iwaypoint)
 
-            check = params.CheckPathAllConstraints(prev_waypoint, curr_waypoint,
-                                                   [], [], 0., OpenStart)
+            check = params.CheckPathAllConstraints(
+                prev_waypoint, curr_waypoint, [], [], 0., OpenStart)
             if check != 0:
                 return True
 
@@ -164,44 +166,44 @@ class PlanToConfigurationTest(object):
         assert_allclose(last_waypoint, self.config_feasible_goal)
         self.assertFalse(self._CollisionCheckPath(path))
 
-    def test_PlanToConfiguration_StartInCollision_ThrowsPlanningError(self):
+    def test_PlanToConfiguration_StartInCollision_Throws(self):
         # Setup
         with self.env:
             self.robot.SetActiveDOFValues(self.config_env_collision)
 
         # Test/Assert
         with self.assertRaises(PlanningError):
-            path = self.planner.PlanToConfiguration(
+            self.planner.PlanToConfiguration(
                 self.robot, self.config_feasible_goal)
 
-    def test_PlanToConfiguration_StartInSelfCollision_ThrowsPlanningError(self):
+    def test_PlanToConfiguration_StartInSelfCollision_Throws(self):
         # Setup
         with self.env:
             self.robot.SetActiveDOFValues(self.config_self_collision)
 
         # Test/Assert
         with self.assertRaises(PlanningError):
-            path = self.planner.PlanToConfiguration(
+            self.planner.PlanToConfiguration(
                 self.robot, self.config_feasible_goal)
 
-    def test_PlanToConfiguration_GoalInCollision_ThrowsPlanningError(self):
+    def test_PlanToConfiguration_GoalInCollision_Throws(self):
         # Setup
         with self.env:
             self.robot.SetActiveDOFValues(self.config_feasible_start)
 
         # Test/Assert
         with self.assertRaises(PlanningError):
-            path = self.planner.PlanToConfiguration(
+            self.planner.PlanToConfiguration(
                 self.robot, self.config_env_collision)
 
-    def test_PlanToConfiguration_GoalInSelfCollision_ThrowsPlanningError(self):
+    def test_PlanToConfiguration_GoalInSelfCollision_Throws(self):
         # Setup
         with self.env:
             self.robot.SetActiveDOFValues(self.config_feasible_start)
 
         # Test/Assert
         with self.assertRaises(PlanningError):
-            path = self.planner.PlanToConfiguration(
+            self.planner.PlanToConfiguration(
                 self.robot, self.config_self_collision)
 
 
@@ -231,7 +233,7 @@ class PlanToEndEffectorPoseTest(object):
         self.assertTransformClose(last_ik, goal_ik)
         self.assertFalse(self._CollisionCheckPath(path))
 
-    def test_PlanToEndEffectorPose_StartInCollision_ThrowsPlanningError(self):
+    def test_PlanToEndEffectorPose_StartInCollision_Throws(self):
         # Setup
         with self.env:
             self.robot.SetActiveDOFValues(self.config_feasible_goal)
@@ -243,7 +245,7 @@ class PlanToEndEffectorPoseTest(object):
         with self.assertRaises(PlanningError):
             self.planner.PlanToEndEffectorPose(self.robot, goal_ik)
 
-    def test_PlanToEndEffectorPose_StartInSelfCollision_ThrowsPlanningError(self):
+    def test_PlanToEndEffectorPose_StartInSelfCollision_Throws(self):
         # Setup
         with self.env:
             self.robot.SetActiveDOFValues(self.config_feasible_goal)
@@ -255,7 +257,7 @@ class PlanToEndEffectorPoseTest(object):
         with self.assertRaises(PlanningError):
             self.planner.PlanToEndEffectorPose(self.robot, goal_ik)
 
-    def test_PlanToEndEffectorPose_GoalInCollision_ThrowsPlanningError(self):
+    def test_PlanToEndEffectorPose_GoalInCollision_Throws(self):
         # Setup
         with self.env:
             self.robot.SetActiveDOFValues(self.config_env_collision)
@@ -267,7 +269,7 @@ class PlanToEndEffectorPoseTest(object):
         with self.assertRaises(PlanningError):
             self.planner.PlanToEndEffectorPose(self.robot, goal_ik)
 
-    def test_PlanToEndEffectorPose_GoalInSelfCollision_ThrowsPlanningError(self):
+    def test_PlanToEndEffectorPose_GoalInSelfCollision_Throws(self):
         # Setup
         with self.env:
             self.robot.SetActiveDOFValues(self.config_self_collision)
@@ -306,33 +308,35 @@ class ShortcutPathTest(object):
 
     # TODO: Test some of the error cases.
 
+
 class RetimeTrajectoryTest(object):
     def setUp(self):
+        cspec = self.robot.GetActiveConfigurationSpecification()
+
         self.feasible_path = openravepy.RaveCreateTrajectory(self.env, '')
-        self.feasible_path.Init(self.robot.GetActiveConfigurationSpecification())
+        self.feasible_path.Init(cspec)
         self.feasible_path.Insert(0, self.waypoint1)
         self.feasible_path.Insert(1, self.waypoint2)
         self.feasible_path.Insert(2, self.waypoint3)
 
         self.dt = 0.01
-        self.tolerance = 0.1 # 10% error
+        self.tolerance = 0.1  # 10% error
 
     def test_RetimeTrajectory(self):
         # Setup/Test
         traj = self.planner.RetimeTrajectory(self.robot, self.feasible_path)
 
         # Assert
-        dof_resolutions = self.robot.GetActiveDOFResolutions()
         position_cspec = self.feasible_path.GetConfigurationSpecification()
         velocity_cspec = position_cspec.ConvertToDerivativeSpecification(1)
         zero_dof_values = numpy.zeros(position_cspec.GetDOF())
 
         # Verify that the trajectory passes through the original waypoints.
-        waypoints = [ self.waypoint1, self.waypoint2, self.waypoint3 ]
-        waypoint_indices = [ None ] * len(waypoints)
+        waypoints = [self.waypoint1, self.waypoint2, self.waypoint3]
+        waypoint_indices = [None] * len(waypoints)
 
         for iwaypoint in xrange(traj.GetNumWaypoints()):
-            joint_values = traj.GetWaypoint(iwaypoint, position_cspec) 
+            joint_values = traj.GetWaypoint(iwaypoint, position_cspec)
 
             # Compare the waypoint against every input waypoint.
             for icandidate, candidate_waypoint in enumerate(waypoints):
@@ -356,12 +360,15 @@ class RetimeTrajectoryTest(object):
             ibefore = iafter - 1
 
             joint_values = traj.Sample(t, position_cspec)
-            joint_values_before = traj.GetWaypoint(ibefore, position_cspec) 
-            joint_values_after = traj.GetWaypoint(iafter, position_cspec) 
-            
-            distance_full = numpy.linalg.norm(joint_values_after - joint_values_before)
-            distance_before = numpy.linalg.norm(joint_values - joint_values_before)
-            distance_after = numpy.linalg.norm(joint_values - joint_values_after)
+            joint_values_before = traj.GetWaypoint(ibefore, position_cspec)
+            joint_values_after = traj.GetWaypoint(iafter, position_cspec)
+
+            distance_full = numpy.linalg.norm(
+                joint_values_after - joint_values_before)
+            distance_before = numpy.linalg.norm(
+                joint_values - joint_values_before)
+            distance_after = numpy.linalg.norm(
+                joint_values - joint_values_after)
             deviation = distance_before + distance_after - distance_full
             self.assertLess(deviation, self.tolerance * distance_full)
 
@@ -381,31 +388,32 @@ class RetimeTrajectoryTest(object):
 # unittest.TestCase class. The unittest.TestCase class MUST APPEAR LAST in the
 # list of base classes.
 
-"""
 class CBiRRTPlannerTests(BasePlannerTest,
                          PlanToConfigurationTest,
                          PlanToEndEffectorPoseTest,
-                         unittest.TestCase): 
+                         unittest.TestCase):
     planner_factory = CBiRRTPlanner
+
 
 class OMPLPlannerTests(BasePlannerTest,
                        PlanToConfigurationTest,
-                       unittest.TestCase): 
+                       unittest.TestCase):
     planner_factory = OMPLPlanner
+
 
 class OMPLSimplifierTests(BasePlannerTest,
                           ShortcutPathTest,
-                          unittest.TestCase): 
+                          unittest.TestCase):
     planner_factory = OMPLSimplifier
 
     def setUp(self):
         BasePlannerTest.setUp(self)
         ShortcutPathTest.setUp(self)
-"""
+
 
 class ParabolicRetimerTests(BasePlannerTest,
                             RetimeTrajectoryTest,
-                            unittest.TestCase): 
+                            unittest.TestCase):
     planner_factory = ParabolicRetimer
 
     def setUp(self):
