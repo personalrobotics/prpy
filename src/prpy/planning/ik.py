63afd2c34755e1bf0d334fee2e3450ca2df0ad31
Pras Velagapudi <pkv@cs.cmu.edu>
2015-01-06 16:39:57 -0500

    Added restructured IK and TSR planners that can do multiple goals.

--- src/prpy/planning/ik.py
+++ src/prpy/planning/ik.py
@@ -3,10 +3,11 @@
 # Copyright (c) 2013, Carnegie Mellon University
 # All rights reserved.
 # Authors: Michael Koval <mkoval@cs.cmu.edu>
+#          Pras Velagapudi <pkv@cs.cmu.edu>
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions are met:
-# 
+#
 # - Redistributions of source code must retain the above copyright notice, this
 #   list of conditions and the following disclaimer.
 # - Redistributions in binary form must reproduce the above copyright notice,
@@ -15,7 +16,7 @@
 # - Neither the name of Carnegie Mellon University nor the names of its
 #   contributors may be used to endorse or promote products derived from this
 #   software without specific prior written permission.
-# 
+#
 # THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
 # AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
 # IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
@@ -27,59 +28,119 @@
 # CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
-import logging, numpy, openravepy
+import logging
+import numpy
 from .. import ik_ranking
-from base import BasePlanner, PlanningError, UnsupportedPlanningError, PlanningMethod
+from base import BasePlanner, PlanningError, PlanningMethod
 
-logger = logging.getLogger('planning.ik')
+logger = logging.getLogger('prpy.planning.ik')
+
 
 class IKPlanner(BasePlanner):
     def __init__(self):
         super(IKPlanner, self).__init__()
 
     def __str__(self):
-        return 'IKConfigurationPlanner'
+        return 'IKPlanner'
 
     @PlanningMethod
-    def PlanToIK(self, robot, goal_pose, ranker=ik_ranking.JointLimitAvoidance, num_attempts=1, **kw_args):
-        import numpy
-        from openravepy import IkFilterOptions, IkParameterization, IkParameterizationType
+    def PlanToIKs(self, robot, goal_poses,
+                  ranker=ik_ranking.JointLimitAvoidance,
+                  num_attempts=1, chunk_size=1, **kw_args):
+        """
+        Create a plan to put a robot active manipulator's end effector at one
+        of the specified affine poses.
+
+        To do this, full IK solutions are computed for each goal pose. These
+        solutions are combined into a single list, which is ranked by the
+        specified ranking function. The solutions are chunked into groups of
+        the specified size and the robot planner is called iteratively on each
+        group in descending ranked order to find a trajectory to reach any
+        solution in the group.
+
+        The planner will terminate when a solution trajectory is found or the
+        robot planner has be called for the specified number of attempts.
+
+        If chunk size = 1, robot.planner.PlanToConfiguration is used.
+        If chunk size >= 1, robot.planner.PlanToConfigurations is used.
+
+        @param robot the robot whose active manipulator will be used
+        @param goal_pose a desired affine transform for the end effector
+        @param ranker an IK ranking function to use over the IK solutions
+        @param num_attempts the number of planning calls before giving up
+        @param chunk_size the number of IK solutions to use per planning call
+        @return traj a trajectory that places the end effector at the goal pose
+        """
+        from openravepy import (IkFilterOptions,
+                                IkParameterization,
+                                IkParameterizationType)
 
         # FIXME: Currently meta-planners duplicate IK ranking in each planning
         # thread. It should be possible to fix this by IK ranking once, then
         # calling PlanToConfiguration in separate threads.
 
-        # Find an unordered list of IK solutions.
+        # Find an unordered list of IK solutions for every specified goal pose.
         with robot.GetEnv():
-            manipulator = robot.GetActiveManipulator()
-            ik_param = IkParameterization(goal_pose, IkParameterizationType.Transform6D)
-            ik_solutions = manipulator.FindIKSolutions(ik_param, IkFilterOptions.CheckEnvCollisions)
+            ik_solutions = []
+            for goal_pose in goal_poses:
+                manipulator = robot.GetActiveManipulator()
+                ik_param = IkParameterization(
+                    goal_pose, IkParameterizationType.Transform6D)
+                ik_solutions += manipulator.FindIKSolutions(
+                    ik_param, IkFilterOptions.CheckEnvCollisions)
 
         if ik_solutions.shape[0] == 0:
             raise PlanningError('There is no IK solution at the goal pose.')
 
         # Sort the IK solutions in ascending order by the costs returned by the
-        # ranker. Lower cost solutions are better and infinite cost solutions are
-        # assumed to be infeasible.
+        # ranker. Lower cost solutions are better and infinite cost solutions
+        # are assumed to be infeasible.
         scores = ranker(robot, ik_solutions)
-        sorted_indices = numpy.argsort(scores)
-        sorted_indices = sorted_indices[~numpy.isposinf(scores)]
-        scores = scores[~numpy.isposinf(scores)]
-        sorted_ik_solutions = ik_solutions[sorted_indices, :]
+        ranked_indices = numpy.argsort(scores)
+        ranked_indices = ranked_indices[~numpy.isposinf(scores)]
+        ranked_ik_solutions = ik_solutions[ranked_indices, :]
 
-        if sorted_ik_solutions.shape[0] == 0:
+        if ranked_ik_solutions.shape[0] == 0:
             raise PlanningError('All IK solutions have infinite cost.')
 
-        # Sequentially plan to the solutions in descending order of cost.
-        num_attempts = min(sorted_ik_solutions.shape[0], num_attempts)
-        for i, ik_solution in enumerate(sorted_ik_solutions[0:num_attempts, :]):
+        # Group the ik solutions into groups of the specified size
+        # (plan for each group of IK solutions together).
+        def chunker(iterable, chunksize):
+            return map(None, *[iter(iterable)]*chunksize)
+        ranked_ik_solution_groups = chunker(ranked_ik_solutions, chunk_size)
+
+        # Sequentially plan to the solution groups in descending cost order.
+        num_attempts = min(ranked_ik_solution_groups.shape[0], num_attempts)
+        ik_group_list = enumerate(ranked_ik_solution_groups[0:num_attempts, :])
+        for i, ik_group in ik_group_list:
             try:
-                traj = robot.planner.PlanToConfiguration(robot, ik_solution)
-                logger.info('Planned to IK solution %d of %d.', i + 1, num_attempts)
+                if ik_group.shape[0] > 1:
+                    traj = robot.planner.PlanToConfigurations(robot, ik_group)
+                else:
+                    traj = robot.planner.PlanToConfiguration(robot, ik_group)
+
+                logger.info('Planned to IK solution %d of %d.',
+                            i + 1, num_attempts)
                 return traj
             except PlanningError as e:
                 logger.warning('Planning to IK solution %d of %d failed: %s',
                                i + 1, num_attempts, e)
 
-        raise PlanningError('Planning to the top {0:d} of {1:d} IK solutions failed.'.format(
-                            num_attempts, sorted_ik_solutions.shape[0]))
+        raise PlanningError(
+            'Planning to the top {:d} of {:d} IK solution failed.'
+            .format(num_attempts, ranked_ik_solution_groups.shape[0]))
+
+    @PlanningMethod
+    def PlanToIK(self, robot, goal_pose, **kw_args):
+        """
+        Create a plan to put a robot active manipulator's end effector at the
+        specified affine pose.
+
+        @param robot the robot whose active manipulator will be used
+        @param goal_pose a desired affine transform for the end effector
+        @param ranker an IK ranking function to use over the IK solutions
+        @param num_attempts the number of planning calls before giving up
+        @param chunk_size the number of IK solutions to use per planning call
+        @return traj a trajectory that places the end effector at the goal pose
+        """
+        return self.PlanToIKs(robot, goal_poses=[goal_pose], **kw_args)
---
+++ src/prpy/planning/tsr.py
@@ -0,0 +1,69 @@
+#!/usr/bin/env python
+
+# Copyright (c) 2013, Carnegie Mellon University
+# All rights reserved.
+# Authors: Pras Velagapudi <pkv@cs.cmu.edu>
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+# - Redistributions of source code must retain the above copyright notice, this
+#   list of conditions and the following disclaimer.
+# - Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# - Neither the name of Carnegie Mellon University nor the names of its
+#   contributors may be used to endorse or promote products derived from this
+#   software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+import logging
+from base import BasePlanner, PlanningMethod
+
+logger = logging.getLogger('prpy.planning.tsr')
+
+
+class TSRPlanner(BasePlanner):
+    def __init__(self):
+        super(TSRPlanner, self).__init__()
+
+    def __str__(self):
+        return 'TSRPlanner'
+
+    @PlanningMethod
+    def PlanToTSR(self, robot, tsrchains,
+                  tsr_samples=10, ranker=None, **kw_args):
+        """
+        Plan to a desired TSR set using a-priori goal sampling.  This planner
+        samples a fixed number of goals from the specified TSRs up-front, then
+        uses robot.planner.PlanToIK to attempt to plan to the resulting affine
+        transformations.
+
+        This planner will return failure if the provided TSR chains require
+        any constraint other than goal sampling.
+
+        @param robot the robot whose active manipulator will be used
+        @param tsrchains a list of TSR chains that define a goal set
+        @param tsr_samples the number of samples of the TSR chains to use
+        @param ranker an IK ranking function to use over the IK solutions
+        @return traj a trajectory that satisfies the specified TSR chains
+        """
+        from ..ik_ranking import NominalConfiguration
+        if ranker is None:
+            ranker = NominalConfiguration(robot.GetActiveDOFValues())
+
+        # TODO: check that the TSR chains only require sample_goal.
+        # TODO: round robin between each TSR chain that has sample_goal.
+        goal_poses = [tsrchains[0].sample() for i in range(tsr_samples)]
+        return robot.planner.PlanToIKs(
+            robot, goal_poses, ranker=ranker, **kw_args)
