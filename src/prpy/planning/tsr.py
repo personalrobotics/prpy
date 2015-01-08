9556e42464dc359f691ca0a7ccd95571ec10efbd
Pras Velagapudi <pkv@cs.cmu.edu>
2015-01-08 12:14:29 -0500

    Implemented TsrPlanner as standalone from IkPlanner.

--- src/prpy/planning/tsr.py
+++ src/prpy/planning/tsr.py
@@ -28,7 +28,10 @@
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 import logging
-from base import BasePlanner, PlanningMethod
+import time
+import itertools
+import numpy
+from base import BasePlanner, PlanningMethod, PlanningError
 
 logger = logging.getLogger('prpy.planning.tsr')
 
@@ -41,8 +44,9 @@ class TSRPlanner(BasePlanner):
         return 'TSRPlanner'
 
     @PlanningMethod
-    def PlanToTSR(self, robot, tsrchains, chunk_size=10,
-                  tsr_samples=10, ranker=None, **kw_args):
+    def PlanToTSR(self, robot, tsrchains, num_attempts=10,
+                  chunk_size=10, ranker=None,
+                  tsr_samples=100, tsr_timeout=5.0, **kw_args):
         """
         Plan to a desired TSR set using a-priori goal sampling.  This planner
         samples a fixed number of goals from the specified TSRs up-front, then
@@ -54,7 +58,9 @@ class TSRPlanner(BasePlanner):
 
         @param robot the robot whose active manipulator will be used
         @param tsrchains a list of TSR chains that define a goal set
-        @param tsr_samples the number of samples of each goal TSR chain to try
+        @param num_attempts the maximum number of planning attempts to make
+        @param tsr_samples the maximum number of samples of the goal TSR chains
+        @param tsr_timeout the maximum time to spend sampling goal TSR chains
         @param chunk_size the number of possible goals to use per planning call
         @param ranker an IK ranking function to use over the IK solutions
         @return traj a trajectory that satisfies the specified TSR chains
@@ -63,9 +69,69 @@ class TSRPlanner(BasePlanner):
         if ranker is None:
             ranker = NominalConfiguration(robot.GetActiveDOFValues())
 
-        # TODO: check that the TSR chains only require sample_goal.
-        # TODO: round robin between each TSR chain that has sample_goal.
-        goal_poses = [tsrchains[0].sample() for i in range(tsr_samples)]
-        return robot.planner.PlanToIKs(
-            robot, goal_poses,
-            ranker=ranker, chunk_size=chunk_size, **kw_args)
+        # Plan using the active manipulator.
+        manipulator = robot.GetActiveManipulator()
+
+        # Create an iterator that will cycle through sampling each TSR chain.
+        tsr_cycler = itertools.cycle(tsrchains)
+
+        # Create an iterator that will sample until the timelimit.
+        tsr_timelimit = time.time() + tsr_timeout
+        tsr_sampler = itertools.takewhile(
+            lambda v: time.time() < tsr_timelimit, tsr_cycler)
+
+        # Sample a list of TSR poses and collate valid IK solutions.
+        from openravepy import (IkFilterOptions,
+                                IkParameterization,
+                                IkParameterizationType)
+        ik_solutions = []
+        for tsrchain in tsr_sampler:
+            ik_param = IkParameterization(
+                tsrchain.sample(), IkParameterizationType.Transform6D)
+            ik_solution = manipulator.FindIKSolutions(
+                ik_param, IkFilterOptions.CheckEnvCollisions)
+            if ik_solution.shape[0] > 0:
+                ik_solutions.append(ik_solution)
+
+        if len(ik_solutions) == 0:
+            raise PlanningError('No collision-free IK solutions at goal TSRs.')
+
+        # Sort the IK solutions in ascending order by the costs returned by the
+        # ranker. Lower cost solutions are better and infinite cost solutions
+        # are assumed to be infeasible.
+        ik_solutions = numpy.vstack(ik_solutions)
+        scores = ranker(robot, ik_solutions)
+        ranked_indices = numpy.argsort(scores)
+        ranked_indices = ranked_indices[~numpy.isposinf(scores)]
+        ranked_ik_solutions = ik_solutions[ranked_indices, :]
+
+        # Group the IK solutions into groups of the specified size
+        # (plan for each group of IK solutions together).
+        ranked_ik_solution_sets = [
+            ranked_ik_solutions[i:i+chunk_size, :]
+            for i in range(0, ranked_ik_solutions.shape[0], chunk_size)
+        ]
+
+        # Sequentially plan to the solution groups in descending cost order.
+        num_attempts = min(len(ranked_ik_solution_sets), num_attempts)
+        ik_set_list = enumerate(ranked_ik_solution_sets[:num_attempts])
+        for i, ik_set in ik_set_list:
+            try:
+                import IPython
+                IPython.embed()
+                if ik_set.shape[0] > 1:
+                    traj = robot.planner.PlanToConfigurations(robot, ik_set)
+                else:
+                    traj = robot.planner.PlanToConfiguration(robot, ik_set[0])
+
+                logger.info('Planned to IK solution set %d of %d.',
+                            i + 1, num_attempts)
+                return traj
+            except PlanningError as e:
+                logger.warning(
+                    'Planning to IK solution set %d of %d failed: %s',
+                    i + 1, num_attempts, e)
+
+        raise PlanningError(
+            'Planning to the top {:d} of {:d} IK solution sets failed.'
+            .format(num_attempts, ranked_ik_solution_sets.shape[0]))
