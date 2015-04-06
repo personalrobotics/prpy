6e3b8297c1524e0c3db485d9e1b3728cca01572b
Siddhartha Srinivasa <siddh@cs.cmu.edu>
2015-04-06 14:09:40 -0400

    Fixed typo
    Changed `prpy.util.CopyTrajectory` to `util.CopyTrajectory` to reflect @mkoval's change to the import.

--- src/prpy/planning/vectorfield.py
+++ src/prpy/planning/vectorfield.py
@@ -244,7 +244,7 @@ class VectorFieldPlanner(BasePlanner):
 
                         status = fn_terminate()
                         if status == Status.CACHE_AND_CONTINUE:
-                            cached_traj = prpy.util.CopyTrajectory(qtraj)
+                            cached_traj = util.CopyTrajectory(qtraj)
                         if status == Status.TERMINATE:
                             break
 
