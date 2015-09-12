4bd4c539e64a8693f9d790f81e88d923dbd62536
Michael Koval <mkoval@cs.cmu.edu>
2015-09-12 18:20:26 -0400

    Check for infinite values in GreedyIK planner.

--- src/prpy/planning/workspace.py
+++ src/prpy/planning/workspace.py
@@ -105,6 +105,8 @@ class GreedyIKPlanner(BasePlanner):
             raise ValueError('Direction must be non-zero')
         elif max_distance is not None and max_distance < distance:
             raise ValueError('Max distance is less than minimum distance.')
+        elif max_distance is not None and not numpy.isfinite(max_distance):
+            raise ValueError('Max distance must be finite.')
 
         # Normalize the direction vector.
         direction = numpy.array(direction, dtype='float')
