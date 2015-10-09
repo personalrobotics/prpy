e63081965a46680657d6d1190ec03631da22aef3
Jennifer King <jeking@andrew.cmu.edu>
2015-10-09 17:53:13 -0400

    Fixing line length

--- src/prpy/base/wam.py
+++ src/prpy/base/wam.py
@@ -237,8 +237,10 @@ class WAM(Manipulator):
         @param max_distance maximum distance in meters
         @param max_force maximum force in Newtons
         @param max_torque maximum torque in Newton-Meters
-        @param ignore_collisions collisions with these objects are ignored when planning the path, e.g. the object you think you will touch
-        @param velocity_limit_scale A multiplier to use to scale velocity limits when executing MoveUntilTouch ( < 1 in most cases).           
+        @param ignore_collisions collisions with these objects are ignored when 
+        planning the path, e.g. the object you think you will touch
+        @param velocity_limit_scale A multiplier to use to scale velocity limits 
+        when executing MoveUntilTouch ( < 1 in most cases).           
         @param **kw_args planner parameters
         @return felt_force flag indicating whether we felt a force.
         """
