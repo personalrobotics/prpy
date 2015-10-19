8ce67f7b6ec7896de00354faf78b1c8571fb8fd3
Gilwoo Lee <gilwool@andrew.cmu.edu>
2015-10-19 18:15:31 -0400

    Modified SnapPlanner and GreedyIKPlanner to raise collision-specific errors. Added tests for collision-specific errors in PlantoConfiguration and PlanToEndEffectorOffset

--- tests/planning/test_SnapPlanner.py
+++ tests/planning/test_SnapPlanner.py
@@ -2,6 +2,7 @@ from methods import (
     PlanToConfigurationTest,
     PlanToConfigurationStraightLineTest,
 )
+from methods.PlanToConfiguration import PlanToConfigurationTestCollisionTest
 from planning_helpers import BasePlannerTest
 from prpy.planning.snap import SnapPlanner
 from unittest import TestCase
@@ -10,6 +11,7 @@ from unittest import TestCase
 class SnapPlannerTest(BasePlannerTest,
                       PlanToConfigurationTest,
                       PlanToConfigurationStraightLineTest,
+                      PlanToConfigurationTestCollisionTest,
                       TestCase):
     planner_factory = SnapPlanner
 
