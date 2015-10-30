20909d633fc5157f77afd4d218a68a715704d3b8
Gilwoo Lee <gilwool@andrew.cmu.edu>
2015-10-30 16:40:31 -0400

    Removed PlanToEndEffectorOffsetTest from TrajoptPlannerTest.

--- tests/planning/test_TrajoptPlanner.py
+++ tests/planning/test_TrajoptPlanner.py
@@ -2,10 +2,8 @@ from methods import (
     PlanToConfigurationTest,
     PlanToConfigurationStraightLineTest,
     PlanToEndEffectorPoseTest,
-    PlanToEndEffectorOffsetTest
 )
 from methods.PlanToConfiguration import PlanToConfigurationTestCollisionTest
-from methods.PlanToEndEffectorOffset import PlanToEndEffectorOffsetCollisionTest
 from planning_helpers import BasePlannerTest
 from unittest import TestCase
 from or_trajopt import TrajoptPlanner
@@ -14,8 +12,6 @@ from or_trajopt import TrajoptPlanner
 class TrajoptPlannerTest(BasePlannerTest,
                         PlanToConfigurationTest,
                         PlanToEndEffectorPoseTest,
-                        PlanToEndEffectorOffsetTest,
-                        PlanToEndEffectorOffsetCollisionTest,
                         PlanToConfigurationTestCollisionTest,
                         TestCase):
     planner_factory = TrajoptPlanner
