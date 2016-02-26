f663c88835abb2b1e27f2d160f1aa7afce150f40
Pras Velagapudi <psigen@gmail.com>
2016-02-25 20:45:36 -0500

    Removed mandatory path simplification to test performance.

--- src/prpy/planning/retimer.py
+++ src/prpy/planning/retimer.py
@@ -95,8 +95,8 @@ class OpenRAVERetimer(BasePlanner):
 
         # Remove co-linear waypoints. Some of the default OpenRAVE retimers do
         # not perform this check internally (e.g. ParabolicTrajectoryRetimer).
-        if not IsTimedTrajectory(output_traj):
-            output_traj = SimplifyTrajectory(output_traj, robot)
+        # if not IsTimedTrajectory(output_traj):
+        #    output_traj = SimplifyTrajectory(output_traj, robot)
 
         # Only collision check the active DOFs.
         dof_indices, _ = cspec.ExtractUsedIndices(robot)
