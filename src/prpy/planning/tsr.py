bc50e9fcb008808d4d0384ce5d9bfd080b12aed6
Gilwoo Lee <gilwoo301@gmail.com>
2016-07-27 22:09:29 -0400

    Removing assertion

--- src/prpy/planning/tsr.py
+++ src/prpy/planning/tsr.py
@@ -111,8 +111,6 @@ class TSRPlanner(BasePlanner):
         # Delegate to robot.planner by default.
         delegate_planner = self.delegate_planner or robot.planner
 
-        assert(num_attempts == 6 and tsr_timeout == 0.7)
-
         # Plan using the active manipulator.
         manipulator = robot.GetActiveManipulator()
 
