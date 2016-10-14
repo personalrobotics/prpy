f94e493083dccd9a5fa12e4896363f9fadadf9af
Gilwoo Lee <gilwoo301@gmail.com>
2016-10-14 12:19:41 -0400

    Removing unused imports

--- tests/planning/test_OpenRAVEPlanner.py
+++ tests/planning/test_OpenRAVEPlanner.py
@@ -1,10 +1,8 @@
 #!/usr/bin/env python
 from methods import (
     PlanToConfigurationTest,
-    PlanToConfigurationStraightLineTest
 )
 from planning_helpers import BasePlannerTest
-from methods.PlanToConfiguration import PlanToConfigurationTestCollisionTest
 from prpy.planning.openrave import OpenRAVEPlanner
 from unittest import TestCase
 
