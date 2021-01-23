e4d2c865cecccf75ac4f04bdb8f4d2e084f39981
Ryan Sinnet <rsinnet@misorobotics.com>
2021-01-22 16:47:57 -0800

    Fix syntax error in meta.py
    Class definitions didn't have colons.

--- src/prpy/perception/meta.py
+++ src/prpy/perception/meta.py
@@ -1,24 +1,24 @@
 #!/usr/bin/env python
 
 import functools
-        
+
 from base import PerceptionModule, PerceptionMethod
 
-class MetaModule(PerceptionModule)
+class MetaModule(PerceptionModule):
     def __init__(self, modules):
         super(PerceptionModule, self).__init__()
         self.modules = modules;
-    
+
     def __str__(self):
         return 'MetaModule'
-        
-class RunAll(MetaModule)
+
+class RunAll(MetaModule):
     def __init__(self, modules):
         super(MetaModule, self).__init__(modules)
-    
+
     def __str__(self):
         return 'RunAll'
-    
+
     @PerceptionMethod
     def DetectObjects(self, robot, **kw_args):
         for module in modules:
